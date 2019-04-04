bdc3dc84de73135df9b0e51a95332895ddcb5885
denzilly <btimmer313@gmail.com>
2019-04-04 16:26:05 +0200

    added comments

--- buttontest.py
+++ buttontest.py
@@ -2,42 +2,52 @@ import RPi.GPIO as GPIO
 import time
 import pygame
 
+
+# Initialise Pi IO Pins
 GPIO.setmode(GPIO.BCM)
 
+#Button pin
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+#LED Pins
 lights = [21, 20, 16]
-
 for x in lights:
     GPIO.setup(x, GPIO.OUT)
 
-
+#Wait parameter, set to 5ms
 wts = .05
 count = 1
 
+#Load audio files
 pygame.init()
 pygame.mixer.init()
 hitsound = pygame.mixer.Sound('ding2.wav')
 winsound = pygame.mixer.Sound('russia2.wav')
 
 
+
+
 while True:
     button_state = GPIO.input(12)
 
-    
+    #if button gets pressed, do this
     if button_state == False:
+        #turn on one additional LED
         for x in range(count):
             GPIO.output(lights[x],True)
-        hitsound.play()   
+        #Hit sound
+        hitsound.play()
         time.sleep(0.2)
         print ("Score is: " + str(count))
         count += 1
-        
+
     if count == 4:
         print("you win")
-        winsound.play()    
+        winsound.play()
+
+        #celebratory LED display
         while count <= 30:
-            
+
             for x in lights:
                 GPIO.output(x, True)
                 time.sleep(wts)
@@ -45,6 +55,6 @@ while True:
                 GPIO.output(x, False)
                 time.sleep(wts)
             count += 1
-            
+
         GPIO.cleanup()
         break
